a9db44d48c0d1851afe331f824c7183ed0c8f709
P SIVA KRISHNA REDDY <34060950+psivakrishnareddy@users.noreply.github.com>
2025-03-03 14:53:34 -0500

    Versioning and Internationalization

--- src/main/java/com/spring/rest/webservices/restful_webservices/user/UserResource.java
+++ src/main/java/com/spring/rest/webservices/restful_webservices/user/UserResource.java
@@ -1,16 +1,26 @@
 package com.spring.rest.webservices.restful_webservices.user;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class UserResource {
 
     private UserDaoService service;
+
+    // Used to get the messages from message.properties internationalization based on accet-language header
+    // the respective language is selected based on the code
+    @Autowired
+    private MessageSource messageSource;
 
 //    This is contructor injection, We do not need @autowored as its by default added.
     public UserResource(UserDaoService service) {
@@ -35,9 +45,21 @@ public class UserResource {
 
     //POST /users
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
+    }
+
+    @GetMapping("/internationalization")
+    public String hello() {
+        Locale locale = LocaleContextHolder.getLocale(); // if the header contains the locale else default is used
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
     }
 }
